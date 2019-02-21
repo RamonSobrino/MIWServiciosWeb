89837c1755675f3d42dd82503854b48f158bd648
carlos andres duarte jimenez <c88duarte@gmail.com>
2019-02-22 00:38:49 +0100

    fix servidor general

--- WS.Recomendation/src/ws/recomendation/webservices/RecomendationWS.java
+++ WS.Recomendation/src/ws/recomendation/webservices/RecomendationWS.java
@@ -8,13 +8,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import ws.recomendation.webservices.model.Place;
 import ws.recomendation.webservices.model.Recomendation;
 import ws.recomendation.webservices.services.RecomendationServices;
 import ws.recomendation.webservices.services.places.RecomendationType;
 
 @Path("rs")
-public class RecomendationRS {
+public class RecomendationWS {
 
     private final RecomendationServices services = new RecomendationServices();
 
@@ -26,23 +25,18 @@ public class RecomendationRS {
 	return result;
     }
 
-    private Recomendation getOviedo() {
+    public Recomendation getOviedo() {
 	Recomendation recomendation = new Recomendation();
 	recomendation.setCity("Oviedo");
 
 	List<RecomendationType> types = new ArrayList<RecomendationType>();
 	types.add(RecomendationType.MUSEUM);
-	services
-		.getPlaces(43.3619145, -5.8493887, types)
-		.stream()
-		.map(this::parceRecomendation)
-		.forEach(recomendation.getPlaces()::add);
+	List<ws.recomendation.webservices.services.places.Recomendation> 
+		places = services.getPlaces(43.3619145, -5.8493887, types);
+	for (ws.recomendation.webservices.services.places.Recomendation p : places) {
+	    recomendation.getPlaces().add(services.parceRecomendation(p));
+	}
 	return recomendation;
-    }
-
-    private Place parceRecomendation(
-	    ws.recomendation.webservices.services.places.Recomendation r) {
-	return new Place(r.getPlace(), r.getAddress(), r.getRating());
     }
 
 }
